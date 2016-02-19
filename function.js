0ed07e92683522788a29bc4065d44abd7445a294
kuramameng <mengjs324@gmail.com>
2016-02-19 09:50:43 -0500

    finish updateStoke algorithm and half of the findChange algorithm

--- function.js
+++ function.js
@@ -10,23 +10,45 @@ var form2object = function(form) {
 };
 
 // define initial stock
-var initStock = {
-  'Twenty Dollar': 20,
-  'Ten Dollar': 20,
-  'Five Dollar': 20,
-  'One Dollar': 20,
-  'Fifty Cent': 20,
-  'Quarter': 20,
-  'Dime': 20,
-  'Nickel': 20,
-  'Penny': 20
+var initStock = {'2000': 20,'1000': 20,'500': 20,'100': 20,'50': 20,'25': 20,'10': 20,'5': 20,'1': 20};
+
+var currencies = [2000, 1000, 500, 100, 50, 25, 10, 5, 1];
+
+var findChange = function (cents){
+    var count = {};
+    currencies.forEach(function(currency){
+        if (cents > 0) {
+          count[currency] = parseInt(cents/currency);
+        } else {
+          count[currency] = 0;
+        }
+        cents -= count[currency]*currency;
+    })
+    return count;
+}
+
+var updateStock = function(count, payment){
+  var update = initStock;
+  // substract change
+  for (var key in count){
+    update[key] -= count[key];
+  }
+
+  // replenish change based on the payment amount
+  currencies.forEach(function(currency){
+    if (parseInt(payment/currency) !== 0){
+        update[currency] += parseInt(payment/currency);
+        payment -= parseInt(payment/currency)*currency;
+    }
+  })
+  return update;
 }
 
 var displayStock = function(stock){
   // empty out stock
   $(".currency-stock").html("");
   for (var key in stock){
-    $(".currency-stock").append("<td id='" + key.replace(" ","-") + "'>" + stock[key] + "</td>");
+    $(".currency-stock").append("<td id='" + key + "'>" + stock[key] + "</td>");
   }
 }
 
@@ -36,9 +58,9 @@ $(document).ready(function(){
   $("#form").on("submit", function(e){
     e.preventDefault();
     var form = form2object(this);
-    var price = Number(form.price);
-    var payment = Number(form.payment);
-    var change = (payment-price).toFixed(2);
+    var price = Number(form.price).toFixed(2)*100;
+    var payment = Number(form.payment).toFixed(2)*100;
+    var change = payment-price;
     if (isNaN(price) || isNaN(payment)) {
       alert("Please enter valid number!");
     }
@@ -46,30 +68,8 @@ $(document).ready(function(){
       alert("You don't have enough money to buy!");
     }
     else{
-      var count = {
-        'Twenty Dollar': 0,
-        'Ten Dollar': 0,
-        'Five Dollar': 0,
-        'One Dollar': 0,
-        'Fifty Cent': 0,
-        'Quarter': 0,
-        'Dime': 0,
-        'Nickel': 0,
-        'Penny': 0
-      };
-      while (change > 0){
-        if(change >= 20 && initStock['Twenty Dollar'] > 0) {initStock['Twenty Dollar']--;count['Twenty Dollar']++;change -= 20;continue;}
-        if(change >= 10 && initStock['Ten Dollar'] > 0) {initStock['Ten Dollar']--;count['Ten Dollar']++;change -= 10;continue;}
-        if(change >= 5 && initStock['Five Dollar'] > 0) {initStock['Five Dollar']--;count['Five Dollar']++;change -= 5;continue;}
-        if(change >= 1 && initStock['One Dollar'] > 0) {initStock['One Dollar']--;count['One Dollar']++;change -= 1;continue;}
-        if(change >= 0.5 && initStock['Fifty Cent'] > 0) {initStock['Fifty Cent']--;count['Fifty Cent']++;change -= 0.5;continue;}
-        if(change >= 0.25 && initStock['Quarter'] > 0) {initStock['Quarter']--;count['Quarter']++;change = (change - 0.25).toFixed(2);continue;}
-        if(change >= 0.1 && initStock['Dime'] > 0) {initStock['Dime']--;count['Dime']++;change = (change - 0.1).toFixed(2);continue;}
-        if(change >= 0.05 && initStock['Nickel'] > 0) {initStock['Nickel']--;count['Nickel']++;change = (change - 0.05).toFixed(2);continue;}
-        if(change >= 0.01 && initStock['Penny'] > 0) {initStock['Penny']--;count['Penny']++;change = (change - 0.01).toFixed(2);continue;}
-        if(change > 0 && initStock['Penny'] === 0) {alert("I don't have enough change! Please replenish!"); break;}
-      }
-      console.log(count);
+      var changeCount = findChange(change);
+      displayStock(updateStock(changeCount,payment));
     }
   })
 
